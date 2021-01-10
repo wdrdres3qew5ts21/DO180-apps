e09aa13a1ed29ce95e6752d9d1951f01eefcc8d8
Naomi Lin <linxianer12@localhost.localdomain>
2021-01-10 18:10:42 +0700

    [ADD] Multistage Build

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/redhat', function (req, res) {
+  res.send('Go to Red Hatter!');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
