7405f888d8d796186d7ca5ca6ef18582fca254cf
Naomi Lin <linxianer12@localhost.localdomain>
2021-01-11 12:03:23 +0700

    [ADD] Detect Priority Command

---
+++ nodejs-app/real.js
@@ -0,0 +1,23 @@
+var createError = require('http-error');
+
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send({
+    "message": `Hello World from pod: ${process.env.HOSTNAME}`,
+    "branch": `S2I (Forget Checkout sorry)`,
+  })
+});
+
+app.get('/detect', function (req, res) {
+  res.send({
+    "message": `Hello World from pod: ${process.env.HOSTNAME}`,
+    "branch": `Detect Script Automatic [I'm Real]`,
+  })
+});
+
+app.listen(9090, function () {
+  console.log('Example app listening on port 9090!');
+});
+
